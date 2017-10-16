2132e0fe5066a9d4b3bbfc8392e81b76274e9ccf
kimjongsoo <soohappykjs7@naver.com>
2017-10-17 03:22:09 +0900

    맛집 메뉴 리스트 뷰로 보이게 만듬
    Item.xml을 만들어 각 리스트마다 어떻게 나올지 정해줌(메뉴 이름, 가격, 사진표시하게 만듬)  사진은 ImageView로 이름 가격은 Textview로 크기와 위치를 textSize gravity로 정하고 아래와 같이 textColor도 넣어줘 세련되게 해줌
    MyAdapter class 만들고 BaseAdapter 상속해 메소드 override해주고 MyItem class생성 RestaurantDetail class 에서 데이터 원본 준비하고 어댑터 생성 및 연결

---
+++ app/src/main/java/com/example/msi/newproject/MyAdapter.java
@@ -0,0 +1,77 @@
+package com.example.msi.newproject;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+/**
+ * Created by soo on 2017-10-17.
+ */
+
+public class MyAdapter extends BaseAdapter {
+    private Context mContext;
+    private int mResource;
+    private ArrayList<MyItem> mItems = new ArrayList<MyItem>();
+
+    public MyAdapter(Context context, int resource, ArrayList<MyItem> items) {
+        mContext = context;
+        mResource = resource;
+        mItems = items;
+    }
+
+    @Override
+    public int getCount() {
+        return mItems.size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return mItems.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+
+        if (convertView == null) {
+            LayoutInflater inflater = (LayoutInflater) mContext.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            convertView = inflater.inflate(mResource, parent,false);
+        }
+        // Set Icon
+        ImageView icon = (ImageView) convertView.findViewById(R.id.imageView);
+        icon.setImageResource(mItems.get(position).mIcon);
+
+        // Set Text 1
+        TextView menu = (TextView) convertView.findViewById(R.id.text1);
+        menu.setText(mItems.get(position).nMenu);
+
+        // Set Text 2
+        TextView price = (TextView) convertView.findViewById(R.id.text2);
+        price.setText(mItems.get(position).nPrice);
+
+        return convertView;
+    }
+
+}
+class MyItem {
+    int mIcon; // image resource
+    String nMenu; // text
+    String nPrice;  // text
+
+    MyItem(int aIcon, String aMenu, String aPrice) {
+        mIcon = aIcon;
+        nMenu = aMenu;
+        nPrice = aPrice;
+    }
+
+}
--- app/src/main/java/com/example/msi/newproject/RestaurantDetail.java
+++ app/src/main/java/com/example/msi/newproject/RestaurantDetail.java
@@ -1,13 +1,38 @@
 package com.example.msi.newproject;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ImageButton;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class RestaurantDetail extends AppCompatActivity {
-
+    static MyAdapter adapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_restaurant_detail);
+        // 데이터 원본 준비
+        ArrayList<MyItem> data = new ArrayList<MyItem>();
+        data.add(new MyItem(R.drawable.sweet, "스윗츄~", "17000"));
+        data.add(new MyItem(R.drawable.boom, "붐바스틱", "18000"));
+        data.add(new MyItem(R.drawable.bring, "뿌링클", "17000"));
+        data.add(new MyItem(R.drawable.matco, "맛초킹", "17000"));
+
+
+        //어댑터 생성
+        adapter = new MyAdapter(this, R.layout.item, data);
+
+        //어댑터 연결
+        ListView listView = (ListView) findViewById(R.id.listView);
+        listView.setAdapter(adapter);
+
+
+
     }
 }
