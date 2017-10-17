96ce62c75ca5a39bc5facb0660d5a3b38ece4724
gofl165 <gofl165@naver.com>
2017-10-17 21:16:18 +0900

    맛집상세보기액티비티에서 값을 전달
    맛집상세보기 액티비티에서 값을 메뉴상세정보 액티비티로 값을 전달을 해주도록 하였습니다.

--- app/src/main/java/com/example/msi/newproject/RestaurantDetail.java
+++ app/src/main/java/com/example/msi/newproject/RestaurantDetail.java
@@ -13,12 +13,16 @@ import java.util.ArrayList;
 
 public class RestaurantDetail extends AppCompatActivity {
     static MyAdapter adapter;
+    ArrayList<MyItem> data = new ArrayList<MyItem>();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_restaurant_detail);
         // 데이터 원본 준비
-        ArrayList<MyItem> data = new ArrayList<MyItem>();
+
+
+
         data.add(new MyItem(R.drawable.sweet, "스윗츄~", "17000"));
         data.add(new MyItem(R.drawable.boom, "붐바스틱", "18000"));
         data.add(new MyItem(R.drawable.bring, "뿌링클", "17000"));
@@ -31,7 +35,57 @@ public class RestaurantDetail extends AppCompatActivity {
         //어댑터 연결
         ListView listView = (ListView) findViewById(R.id.listView);
         listView.setAdapter(adapter);
+        //아래메뉴 누르면 MenuDetail으로 Activity convert
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            public void onItemClick(AdapterView<?> parent, View vClicked,
+                                    int position, long id) {
 
+
+
+                Intent intent = new Intent(getApplicationContext(), MenuDetail.class);
+
+
+
+                intent.putExtra("menu", data.get(position).nMenu);
+                intent.putExtra("img", data.get(position).mIcon);
+                intent.putExtra("price", data.get(position).nPrice);
+
+
+                startActivity(intent);
+
+            }
+        });
+        //통화버튼누르면 Dail 연결
+        ImageButton btn = (ImageButton) findViewById(R.id.dial);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent implicit_intent = new Intent(Intent.ACTION_DIAL, Uri.parse("tel:02-908-8005"));
+                startActivity(implicit_intent);
+            }
+
+=======
+>>>>>>> d737f8040287f94747b52aa98ac51f1676699fa6
+
+        });
+
+                Intent intent = new Intent(getApplicationContext(), MenuDetail.class);
+                startActivity(intent);
+
+            }
+
+
+
+        ImageButton btn = (ImageButton) findViewById(R.id.dial);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent implicit_intent = new Intent(Intent.ACTION_DIAL, Uri.parse("tel:02-908-8005"));
+                startActivity(implicit_intent);
+            }
+
+
+        });
 
 
     }
