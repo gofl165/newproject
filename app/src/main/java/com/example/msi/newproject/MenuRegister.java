e2b55f769363ddbd2ad54e4ce3568e1d65053a0f
jongsoo <soohappykjs7@naver.com>
2017-11-18 07:39:30 +0900

    MenuRegister
    1.버튼 눌러 촬영된 영상을 가져와 이미지 버튼에 표시한다.
    ->누르면 dispatchTakePictureIntent()실행 ,onActivityResult()통해 ImageButton에 촬영된 사진 설정
    2.버튼을 눌렀을 때, 데이터베이스에 맛집 정보를 저장한 후, (RestaurantDetail) 액티비티로 이동
    ->데이터베이스 저장위해  Sharedpreference사용

--- app/src/main/java/com/example/msi/newproject/MenuRegister.java
+++ app/src/main/java/com/example/msi/newproject/MenuRegister.java
@@ -1,13 +1,152 @@
 package com.example.msi.newproject;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.net.Uri;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.support.v4.content.FileProvider;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import java.io.File;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class MenuRegister extends AppCompatActivity {
+
+//    private DBHelper mDbHelper;
+    public static final String PREFERENCES_1 = "info";
+
+    public static final String PREFERENCES_ATTR1 = "name";
+    public static final String PREFERENCES_ATTR2 = "t1";
+    public static final String PREFERENCES_ATTR3 = "t2";
+    public static final String PREFERENCES_IMG = "img";
+    SharedPreferences setting;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu_register);
+        ImageButton imageCaptureBtn = (ImageButton)findViewById(R.id.imageCaptureBtn);
+        imageCaptureBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                dispatchTakePictureIntent();
+            }
+        });
+
+        Button button = (Button)findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+
+                EditText main=(EditText) findViewById(R.id.name);
+                ImageView img=(ImageView)findViewById(R.id.imageCaptureBtn);
+                EditText t1=(EditText) findViewById(R.id.address);
+                EditText t2=(EditText) findViewById(R.id.phone);
+
+                setting = getSharedPreferences(PREFERENCES_1, MODE_PRIVATE);
+
+                SharedPreferences.Editor editor = setting.edit();
+
+                String name = main.getText().toString();
+                editor.putString(PREFERENCES_ATTR1, name);
+
+
+                String a = t1.getText().toString();
+                editor.putString(PREFERENCES_ATTR2, a);
+
+
+                String b = t2.getText().toString();
+                editor.putString(PREFERENCES_ATTR3, b);
+//               img bit로 바꿔서 저장하려다 포기
+//                Bitmap bit;
+//                ByteArrayOutputStream byteArrayOutputStream=new ByteArrayOutputStream();
+//                bit.compress(Bitmap.CompressFormat.JPEG,100,byteArrayOutputStream);
+//                byte[] imageBytes=byteArrayOutputStream.toByteArray();
+//                Base64.encodeToString(imageBytes,Base64.NO_WRAP);
+
+                String c =mPhotoFileName;
+                editor.putString(PREFERENCES_IMG,c);
+
+                editor.commit();
+
+
+        Intent intent =new Intent(getApplicationContext(),RestaurantDetail.class);
+        startActivity(intent);
+            }
+        });
+
+
+
+
+
+//        mDbHelper = new DBHelper(this);
+
     }
+
+    String mPhotoFileName;
+    File mPhotoFile;
+    static final int REQUEST_IMAGE_CAPTURE = 1;
+    private String currentDateFormat(){
+        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyyMMdd_HH_mm_ss");
+        String  currentTimeStamp = dateFormat.format(new Date());
+        return currentTimeStamp;
+    }
+    private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            //1. 카메라 앱으로 찍은 이미지를 저장할 파일 객체 생성
+            mPhotoFileName = "IMG"+currentDateFormat()+".jpg";
+            mPhotoFile = new File(getExternalFilesDir(Environment.DIRECTORY_PICTURES), mPhotoFileName);
+
+            if (mPhotoFile !=null) {
+                //2. 생성된 파일 객체에 대한 Uri 객체를 얻기
+                Uri imageUri = FileProvider.getUriForFile(this, "com.example.msi.newproject", mPhotoFile);
+                //authority에 패키지이름 고쳐주기
+                //3. Uri 객체를 Extras를 통해 카메라 앱으로 전달
+                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT,imageUri);
+                startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+            } else
+                Toast.makeText(getApplicationContext(), "file null", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent data){
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            if (mPhotoFileName != null) {
+                mPhotoFile = new File(getExternalFilesDir(Environment.DIRECTORY_PICTURES), mPhotoFileName);
+                ImageButton imageCaptureBtn = (ImageButton)findViewById(R.id.imageCaptureBtn);
+
+                imageCaptureBtn.setImageURI(Uri.fromFile(mPhotoFile));
+
+            }
+        }
+
+    }
+//                        아래 database 관련 나중에 쓸것같아서 일단 주석처리
+//    private void insertRecord() {
+//        EditText name = (EditText)findViewById(R.id.name);
+//        EditText address = (EditText)findViewById(R.id.address);
+//        EditText phone = (EditText)findViewById(R.id.phone);
+//        long nOfRows = mDbHelper.insertUserByMethod(name.getText().toString(),address.getText().toString(),phone.getText().toString());
+//        if (nOfRows >0)
+//            Toast.makeText(this,nOfRows+" Record Inserted", Toast.LENGTH_SHORT).show();
+//        else
+//            Toast.makeText(this,"No Record Inserted", Toast.LENGTH_SHORT).show();
+//    }
+
+
+
+
 }
--- app/src/main/java/com/example/msi/newproject/RestaurantDetail.java
+++ app/src/main/java/com/example/msi/newproject/RestaurantDetail.java
@@ -1,74 +1,184 @@
 package com.example.msi.newproject;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.database.Cursor;
 import android.net.Uri;
+import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Adapter;
 import android.widget.AdapterView;
 import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.ListView;
+import android.widget.SimpleCursorAdapter;
+import android.widget.TextView;
 
+import java.io.File;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 
 public class RestaurantDetail extends AppCompatActivity {
     static MyAdapter adapter;
-    ArrayList<MyItem> data = new ArrayList<MyItem>();
+//    ArrayList<MyItem> data = new ArrayList<MyItem>();
+//    private DBHelper mDbHelper;
+
+    public static final String PREFERENCES_1 = "info";
+
+    public static final String PREFERENCES_ATTR1 = "name";
+    public static final String PREFERENCES_ATTR2 = "t1";
+    public static final String PREFERENCES_ATTR3 = "t2";
+    public static final String PREFERENCES_IMG = "img";
+    SharedPreferences setting;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_restaurant_detail);
-        // 데이터 원본 준비
+//        mDbHelper = new DBHelper(this);
+//        ***********************과제1*****************************
+//        // 데이터 원본 준비
+//        data.add(new MyItem(R.drawable.sweet, "스윗츄~", "17000"));
+//        data.add(new MyItem(R.drawable.boom, "붐바스틱", "18000"));
+//        data.add(new MyItem(R.drawable.bring, "뿌링클", "17000"));
+//        data.add(new MyItem(R.drawable.matco, "맛초킹", "17000"));
+//
+//
+//        //어댑터 생성
+//        adapter = new MyAdapter(this, R.layout.item, data);
+//
+//        //어댑터 연결
+//        ListView listView = (ListView) findViewById(R.id.listView);
+//        listView.setAdapter(adapter);
+//        //아래메뉴 누르면 MenuDetail으로 Activity convert
+//        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+//            public void onItemClick(AdapterView<?> parent, View vClicked,
+//                                    int position, long id) {
+//
+//
+//
+//
+//                Intent intent = new Intent(getApplicationContext(), MenuDetail.class);
+//
+//
+//
+//                intent.putExtra("menu", data.get(position).nMenu);
+//                intent.putExtra("img", data.get(position).mIcon);
+//                intent.putExtra("price", data.get(position).nPrice);
+//
+//
+//                startActivity(intent);
+//
+//            }
+//        });
+//        //통화버튼누르면 Dail 연결
+//        ImageButton btn = (ImageButton) findViewById(R.id.dial);
+//        btn.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Intent implicit_intent = new Intent(Intent.ACTION_DIAL, Uri.parse("tel:02-908-8005"));
+//                startActivity(implicit_intent);
+//            }
+//
+//
+//
+//        });
+//        ******************************************************************
+        TextView main=(TextView)findViewById(R.id.bhc);
+        ImageView img=(ImageView)findViewById(R.id.imageView2);
+        TextView t1=(TextView)findViewById(R.id.location);
+        TextView t2=(TextView)findViewById(R.id.phonenumber);
+        TextView t3=(TextView)findViewById(R.id.whattime);
 
 
-        data.add(new MyItem(R.drawable.sweet, "스윗츄~", "17000"));
-        data.add(new MyItem(R.drawable.boom, "붐바스틱", "18000"));
-        data.add(new MyItem(R.drawable.bring, "뿌링클", "17000"));
-        data.add(new MyItem(R.drawable.matco, "맛초킹", "17000"));
 
 
-        //어댑터 생성
-        adapter = new MyAdapter(this, R.layout.item, data);
+//        **************************data받기*******************
+        setting = getSharedPreferences(PREFERENCES_1, MODE_PRIVATE);
+          String nameText = setting.getString(PREFERENCES_ATTR1, "");
+        String nameText2 = setting.getString(PREFERENCES_ATTR2, "");
+        String nameText3 = setting.getString(PREFERENCES_ATTR3, "");
+        String image=setting.getString(PREFERENCES_IMG,"");
+        File mPhotoFile = new File(getExternalFilesDir(Environment.DIRECTORY_PICTURES), image);
 
-        //어댑터 연결
-        ListView listView = (ListView) findViewById(R.id.listView);
-        listView.setAdapter(adapter);
-        //아래메뉴 누르면 MenuDetail으로 Activity convert
-        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            public void onItemClick(AdapterView<?> parent, View vClicked,
-                                    int position, long id) {
-
+        main.setText(nameText);
+        t1.setText(nameText2);
+        t2.setText(nameText3);
+        img.setImageURI(Uri.fromFile(mPhotoFile));
 
 
 
-                Intent intent = new Intent(getApplicationContext(), MenuDetail.class);
 
 
 
-                intent.putExtra("menu", data.get(position).nMenu);
-                intent.putExtra("img", data.get(position).mIcon);
-                intent.putExtra("price", data.get(position).nPrice);
+//        ******************************************************
+
+//        Cursor cursor = mDbHelper.getAllUsersByMethod();
+//        main.setText(((Cursor)adapter.getItem(i)).getString(0));
+//        img.setImageResource(((Cursor)adapter.getItem(i)).getString(1));
+//        File mPhotoFile;
+//        String mPhotoFileName;
+//
+//        mPhotoFileName = "IMG"+currentDateFormat()+".jpg";
+//        mPhotoFile = new File(getExternalFilesDir(Environment.DIRECTORY_PICTURES), mPhotoFileName);
+//        img.setImageURI(Uri.fromFile(mPhotoFile));
+//        t1.setText(((Cursor)adapter.getItem(i)).getString(2));
+//        t2.setText(((Cursor)adapter.getItem(i)).getString(2));
+//        t3.setText(((Cursor)adapter.getItem(i)).getString(2));
 
 
-                startActivity(intent);
-
-            }
-        });
         //통화버튼누르면 Dail 연결
         ImageButton btn = (ImageButton) findViewById(R.id.dial);
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent implicit_intent = new Intent(Intent.ACTION_DIAL, Uri.parse("tel:02-908-8005"));
+                String nameText3 = setting.getString(PREFERENCES_ATTR3, "");
+                Intent implicit_intent = new Intent(Intent.ACTION_DIAL, Uri.parse("tel:"+nameText3));
                 startActivity(implicit_intent);
             }
 
 
 
         });
-        
-
 
         }
+
+//    private String currentDateFormat(){
+//        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyyMMdd_HH_mm_ss");
+//        String  currentTimeStamp = dateFormat.format(new Date());
+//        return currentTimeStamp;
+//    }
+//
+//
+//    private void viewAllToListView() {
+//
+//        Cursor cursor = mDbHelper.getAllUsersByMethod();
+//
+//        SimpleCursorAdapter adapter = new SimpleCursorAdapter(getApplicationContext(),
+//                R.layout.item, cursor, new String[]{
+//                UserContract.Users.KEY_NAME,
+//                UserContract.Users.KEY_ADDRESS,
+//                UserContract.Users.KEY_PHONE},
+//                new int[]{ R.id.name,R.id.address, R.id.phone}, 0);
+//
+//        ListView lv = (ListView)findViewById(R.id.listView);
+//        lv.setAdapter(adapter);
+//
+//        lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+//            @Override
+//            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+//                Adapter adapter = adapterView.getAdapter();
+//
+//                mId.setText(((Cursor)adapter.getItem(i)).getString(0));
+//                mName.setText(((Cursor)adapter.getItem(i)).getString(1));
+//                mPhone.setText(((Cursor)adapter.getItem(i)).getString(2));
+//            }
+//        });
+//        lv.setChoiceMode(ListView.CHOICE_MODE_SINGLE);
+//    }
+
 }
