977ba602eb726a619cab2a76faf04b3b9144f806
gofl165 <gofl165@naver.com>
2017-11-21 21:27:27 +0900

    listview에서 항목선택된 항목번호가 넘어와 index가 되었고 String값은 넘겨준값 그대로 적용하였고 파일객체를 만들어 절대경로로 파일경로 알려주고 데이터베이스에 사진이름저장된것을 받아와  절대경로 마지막 파일명으로 만들었다.그다음 setImageURI로 이미지 설정

--- app/src/main/java/com/example/msi/newproject/FrgMenuDetail.java
+++ app/src/main/java/com/example/msi/newproject/FrgMenuDetail.java
@@ -32,7 +32,36 @@ public class FrgMenuDetail extends Fragment {
 
 
         View view = inflater.inflate(R.layout.fragment_frg_menu_detail, container, false);
-        ////////////
+
+
+        TextView menu = (TextView)view.findViewById(R.id.text1);
+        TextView price = (TextView)view.findViewById(R.id.text2);
+        TextView detail=(TextView)view.findViewById(R.id.text3);
+        ImageView img = (ImageView)view.findViewById(R.id.imageView);
+        menu.setText(a);
+        price.setText(b);
+        //img.setImageResource(c);
+
+        detail.setText(d);
+        if (index >=0) {
+            String t1=getArguments().getString("menu2");
+            String t2=getArguments().getString("price2");
+            String t3=getArguments().getString("detail2");
+            String t4=getArguments().getString("imgname");
+            menu.setText(t1);
+            price.setText(t2);
+            detail.setText(t3);
+            File F=new File("storage/emulated/0/Android/data/com.example.msi.newproject/files/Pictures",t4);
+            img.setImageURI(Uri.fromFile(F));
+//            img.setImageResource(R.drawable.boom);
+
+        }
+        else{
+            File F=new File("storage/emulated/0/Android/data/com.example.msi.newproject/files/Pictures",c);
+            img.setImageURI(Uri.fromFile(F));
+        }
+
+
         return view;
     }
     //MenuDetail 에서 인텐트로받은 String,int값
