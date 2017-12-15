1fde77f4feac7dc76c56e9d90208ddebee40d26d
jongsoo <soohappykjs7@naver.com>
2017-12-15 15:56:18 +0900

    애니메이션수정
    드로워블 에니메이션,프로퍼티 에니메이션 사용하였고, 끝나고 액티비티변환위해 애니메이션 리스너 사용..

--- app/src/main/java/com/example/msi/newproject/Animate.java
+++ app/src/main/java/com/example/msi/newproject/Animate.java
@@ -1,21 +1,78 @@
 package com.example.msi.newproject;
 
+import android.animation.Animator;
+import android.animation.AnimatorSet;
+import android.animation.ObjectAnimator;
+import android.content.Intent;
 import android.graphics.drawable.AnimationDrawable;
 import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
 import android.widget.ImageView;
 
-/**
- * Created by msi on 2017-12-12.
- */
+
 
 public class Animate extends AppCompatActivity {
+    ImageView mCountDown ;
+    ImageView img;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_animate);
 
-    ImageView mCountDown;
-    private void startCountDownFrameAnimation() {
-        mCountDown.setBackgroundResource(R.drawable.frame_anim);
-        AnimationDrawable countdownAnim = (AnimationDrawable) mCountDown.getBackground();
-        countdownAnim.start();
+        mCountDown = (ImageView) findViewById(R.id.countdown);
+
+
     }
-
+protected void onResume(){
+        super.onResume();
+    startCountDownFrameAnimation();
+    startRocketObjectPropertyAnimation();
 
 }
+
+
+    private void startCountDownFrameAnimation() {
+/*1*/    mCountDown.setBackgroundResource(R.drawable.frame_anim);
+
+/*2*/    AnimationDrawable countdownAnim =
+                (AnimationDrawable)mCountDown.getBackground();
+/*3*/    countdownAnim.start();
+    }
+
+    private void startRocketObjectPropertyAnimation() {
+
+        ObjectAnimator alpha = ObjectAnimator.ofFloat(mCountDown, "alpha", 1, 0);
+
+
+        AnimatorSet animatorSet = new AnimatorSet();
+        animatorSet.play(alpha);
+
+        animatorSet.setDuration(2000);
+        animatorSet.setStartDelay(2500);
+        animatorSet.start();
+        animatorSet.addListener(animatorListener);
+    }
+        Animator.AnimatorListener animatorListener = new Animator.AnimatorListener() {
+            @Override
+            public void onAnimationStart(Animator animator) {
+
+            }
+
+            @Override
+            public void onAnimationEnd(Animator animator) {
+                finish();
+                startActivity(new Intent(getApplicationContext(), RestaurantMap.class));
+            }
+
+            @Override
+            public void onAnimationCancel(Animator animator) {
+
+            }
+
+            @Override
+            public void onAnimationRepeat(Animator animator) {
+
+            }
+        };
+    }
+
